c5ec3ce85b893bc712143e98871a57418f9ed714
Deni Prasetyo <deni.prasetyo@solecode.id>
2026-02-17 19:44:58 +0700

    Add Sitemap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,11 @@
         </script>
 
         <meta name="google-site-verification" content="Vz-00hihw08wzLvV8jnHr6nGyTXwvX-UZlZs806dySY" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
     </head>
 
     <body class="font-primary">
